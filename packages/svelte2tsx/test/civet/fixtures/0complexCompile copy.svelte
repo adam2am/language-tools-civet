461309efd734783602359958187785e6f0b207d4
adam2am <adam2am@outlook.com>
2025-06-27 10:56:32 -0700

    test

---
+++ packages/svelte2tsx/test/civet/fixtures/0complexCompile copy.svelte
@@ -0,0 +1,10 @@
+<script lang="civet">
+	abc:number|null .= 1
+	queryFun2 := (query:number) ->
+		abc = if abc is query then null else query
+
+	fruits := ['apple', 'banana', 'cherry']
+    // First loop, should generate 'i'
+	for fruit, index of fruits
+		abc = index
+</script> 
